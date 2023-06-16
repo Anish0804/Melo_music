55a681ba282e7b715c68b07ce12dffc0b8ee796b
AkhilBabu <akhilbabu.mpn@gmail.com>
2023-06-16 07:51:10 +0530

    marketplace deploy updated

--- scripts/aideploy.js
+++ scripts/aideploy.js
@@ -1,11 +1,15 @@
 const hre = require("hardhat");
-
+const ethers=require('ethers');
 async function main() {
   const NAME = "AI Generated NFT"
   const SYMBOL = "AINFT"
   const COST = ethers.utils.parseUnits("1", "ether") // 1 ETH
 
-  const NFT = await hre.ethers.getContractFactory("ai_NFT")
+  //const NFT = await hre.ethers.getContractFactory("ai_NFT")
+  const NFT = await hre.ethers.getContractFactory("ai_NFT", {
+    filePath: "contracts/ai_NFT.sol" // Provide the path address to the contract file
+  });
+  
   const nft = await NFT.deploy(NAME, SYMBOL, COST)
   await nft.deployed()
 
